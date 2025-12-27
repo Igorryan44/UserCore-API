18c34ec1b35e0f8f4c882998a2eaf94dd16dda6e
Igorryan44 <igorryanmendesalves@gmail.com>
2025-12-27 20:16:32 -0300

    Projeto UserCore - API

---
+++ src/main/java/com/igor_dev/user_core_api/UserCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.igor_dev.user_core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCoreApiApplication.class, args);
+	}
+
+}
